cc416980310ae849e00967dcfc8282e1fd43af5f
ChristophorusX <mrxrtxrt@gmail.com>
2018-03-11 10:14:41 -0400

    fix bugs

--- models.py
+++ models.py
@@ -33,6 +33,12 @@ class BasicBlock(nn.Module):
         return out
 
 
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+
 class ResNet(nn.Module):
 
     def __init__(self, block, layers, num_classes=1000):
